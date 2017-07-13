d393ec1a91482e9eda86dac7ee8eda47929f6a74
Ariful Haque <arifulhaque@Arifuls-MacBook-Pro.local>
2017-07-12 21:17:03 -0400

    setting up basic evevent handlers between Components and main App

--- src/App/index.js
+++ src/App/index.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
 import './styles.css';
+
 import Data from './Data';
 import Filters from './Components/Filters';
 import TableScroller from './Components/TableScroller';
 
-
-/**
- * 
- */
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = { data:  Data.findAll() }; // temporary
+    this.handleFilterResults = this.handleFilterResults.bind(this);
+  }
+
+  handleFilterResults(res) {
+    let data = Data.findAll();
+    this.setState({ data: });
+  }
+
   render() {
     return (
-      <div className="App">
-        <Filters />
-        <TableScroller />
+      <div id="container">
+        <div className="leftPane">
+          <Filters onResults={this.handleFilterResults} />
+        </div>
+        <div className="rightPane">
+          <TableScroller data={this.state.data} />
+        </div>
       </div>
     );
   }
