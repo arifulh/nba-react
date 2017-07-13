0a181ffacaa93d6c2d957018a0d7eadb5a3209c3
Ariful Haque <arifulhaque@Arifuls-MacBook-Pro.local>
2017-07-12 21:53:21 -0400

    set up basic table row rendering

--- src/App/Components/TableScroller/index.js
+++ src/App/Components/TableScroller/index.js
@@ -2,9 +2,32 @@ import React, { Component } from 'react';
 import './styles.css';
 
 class TableScroller extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      currentTop: 0,
+      currentBot: 0,
+      isScrolling: false
+    }
+    this.onScrollingHandler = this.onScrollingHandler.bind(this);    
+  }
+
+  onScrollingHandler(e) {
+
+  }
+
   render() {
+    const rows = this.props.data.map((p, i) => {
+      let cells = Object.keys(p).map((key)  => {
+        if (key !== 'meta' && key !== 'position' && key !== 'team') return (<div>{p[key]}</div>)
+      })
+      return (<div className="table-row"> { cells } </div>)
+    });
+
     return (
       <div className="TableScroller">
+        <div className="TableBody">{ rows }</div>
       </div>
     );
   }
